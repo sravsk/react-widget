02f97431819d4467913443790d10d4f4e4646140
sravsk <sravanthi.karanam@gmail.com>
2018-07-12 14:31:08 -0700

    basic widget component setup

--- webpack.config.js
+++ webpack.config.js
@@ -4,20 +4,23 @@ var DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
   mode: 'production',
-  entry: `${SRC_DIR}/index.jsx`,
+  entry: `${SRC_DIR}/index.js`,
   output: {
     filename: 'bundle.js',
     path: DIST_DIR
+  },
+  serve: {
+    content: DIST_DIR
   },
   module: {
     rules: [
       {
         test: /\.jsx?/,
         include: SRC_DIR,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015']
-        }
+        loader: 'babel-loader'
+        // query: {
+        //   presets: ['react', 'es2015']
+        // }
       },
       {
         test: /\.css$/,
