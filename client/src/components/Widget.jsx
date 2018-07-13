64ab09e9576c9ae1a772e0c191c4ed1244dabdfa
sravsk <sravanthi.karanam@gmail.com>
2018-07-12 22:17:59 -0700

    installed react-transition-group to transition when widget leaves and enters third party site DOM

--- client/src/components/Widget.jsx
+++ client/src/components/Widget.jsx
@@ -6,15 +6,17 @@ import '../../styles/widget-style.css'
 class Widget extends React.Component {
 	constructor(props){
 		super(props);
+		this.state = {
+			open : false
+		}
 	}
 
 	render() {
 		return (
 			<div className="docked-widget">
 				<Row className="widget-dialog">
-					<Col className="widget-title">Hi Sravanthi! </Col>
-					<span className="company-title">Salesforce Knowledge base</span>
-					
+				<Col className="widget-title">Hi Sravanthi! </Col>
+				<span className="company-title">Salesforce Knowledge base</span>
 				</Row>
 				<Row className="widget-body">
 				<Col className="body-title">Advice and anssers from Salesforce</Col>
