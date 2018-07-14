fb6261904fd44189e24ae1f3adddf09187088075
sravsk <sravanthi.karanam@gmail.com>
2018-07-14 16:22:16 -0700

    set the webpack mode to development instead of production so that webpack would not waste resources on UglifyJSPlugin and minifying assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-var path = require('path');
-var webpack = require('webpack');
-var SRC_DIR = path.join(__dirname, '/client/src');
-var DIST_DIR = path.join(__dirname, '/client/dist');
+const path = require('path');
+const webpack = require('webpack');
+const MinifyPlugin = require("babel-minify-webpack-plugin");
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
-  mode: 'production',
-  devtool: 'source-map',
+  mode: 'development',
   //entry: `${SRC_DIR}/index.js`,
   entry: './client/src/outputs/Embeddable-Widget.js',
   output: {
@@ -18,17 +18,7 @@ module.exports = {
     libraryTarget: 'window',
   },
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false
-      }
-    })
+    new MinifyPlugin()
   ],
   serve: {
     content: DIST_DIR
