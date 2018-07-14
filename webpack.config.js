2d6382a7008c7f0da370c9754c5c86ed4e04ee86
sravsk <sravanthi.karanam@gmail.com>
2018-07-14 12:48:21 -0700

    treeshaking only setup for es2015 modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 var path = require('path');
+var webpack = require('webpack');
 var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
   mode: 'production',
+  devtool: 'source-map',
   //entry: `${SRC_DIR}/index.js`,
   entry: './client/src/outputs/Embeddable-Widget.js',
   output: {
@@ -15,6 +17,19 @@ module.exports = {
     libraryExport: 'default',
     libraryTarget: 'window',
   },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ],
   serve: {
     content: DIST_DIR
   },
@@ -25,7 +40,7 @@ module.exports = {
         include: SRC_DIR,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015'],
+          presets: [["es2015", { "modules": false }], "react"],
           plugins: ['transform-class-properties']
         }
       },
