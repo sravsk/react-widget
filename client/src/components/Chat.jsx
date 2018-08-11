bcb396cd0ca62d5f1e67d3e294b31f9d6c7f24b0
sravsk <sravanthi.karanam@gmail.com>
2018-08-11 12:05:52 -0700

    style the chat component

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -19,12 +19,13 @@ class Chat extends React.Component{
 		return(
 			<div>
 				<div className="knowhow-chat-box">
-					<span className="knowhow-chat-title">Start a conversation</span>
-					<div className="knowhow-chat-newConversation">New Conversation
+					<span className="knowhow-chat-title">Start a conversation</span><br/>
+					<div className="knowhow-chat-newConversation" onClick={this.openChatService}>
 					<Icon 
 	          type="wechat" 
 	          style={{ fontSize: 22}} 
-	          className="dock-button" onClick={this.openChatService}/>
+	          className="dock-button"/>
+					<span>New Conversation</span>
 	      	</div>
 				</div>
 				<div className={this.state.renderChat}></div>
